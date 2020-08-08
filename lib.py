eb4c485436214a38ae33f6aa6df848f9a73d5adb
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2020-08-08 19:54:41 -0400

    basic CLI creation/saving/application of watermarks

--- lib.py
+++ lib.py
@@ -20,7 +20,9 @@ from stegano import lsb
 # Macros
 
 PRINTORDER = ['id', 'author', 'client', 'platform', 'date']
+FILETYPES = ['.jpg', '.jpeg', '.png']
 DATE = date.today()
+TEMPPATH = scriptpath() + "/config/templates"
 
 # Class Definitions
 
@@ -60,6 +62,7 @@ class WaterMark:
 def write_wm(wm, ifpath, ofpath):
     encoded = lsb.hide(ifpath, wm.dumpjson())
     encoded.save(ofpath)
+    return 0
 
 def read_wm(wm, ifpath):
     """ Returns 2 for STANDARD message read. Returns 3 for CUSTOM message read.
@@ -93,6 +96,36 @@ def clear():
     if os.name == 'nt': _ = os.system('cls')
     else: _ = os.system('clear')
 
+def read_templates(path):
+    with open(path, 'r') as f:
+        try: return json.loads(f.read())
+        except:
+            err_doc_read("templates file")
+            return {}
+
+def write_templates(path, templates):
+    with open(path, 'w') as f:
+        try: f.write(json.dumps(templates))
+        except:
+            err_doc_read("templates file")
+            return 1
+        return 0
+
+def file_exists(path):
+    if os.path.exists(path): return True
+    else: return err_file_nexist(path)
+
+def file_type(path):
+    global FILETYPES
+    if os.path.splitext(path)[1] in FILETYPES: return True
+    else: return err_file_type_nsupported(os.path.splitext(path)[1])
+
+def nfile_direxists(path):
+    if os.path.split(path)[0] == '': return True
+    else:
+        if os.path.isdir(os.path.split(path)[0]): return True
+        else: return err_invalid_dir(path)
+    
 # Error Messages
 
 def err_type_mismatch(var, desired_type):
@@ -103,3 +136,20 @@ def err_type_mismatch(var, desired_type):
 def err_invalid_command(command):
     print("Error: {} is not a valid command. Press 'h' for more help."\
           .format(command))
+    return False
+
+def err_doc_read(id):
+    print("Error reading {}. Some functionality may not be available."\
+          .format(id))
+    return False
+
+def err_file_nexist(path):
+    print("Error: File '{}' does not exist.".format(path))
+    return False
+
+def err_file_type_nsupported(ext):
+    print("Error: {} files are not supported at this time.".format(ext))
+    return False
+
+def err_invalid_dir(path):
+    print("Error: the file {} does not have a valid directory path.".format(path))
--- ma.py
+++ ma.py
@@ -1,6 +1,6 @@
 """
-ma.py
 ma
+ma.py
 
 Kat Cannon-MacMartin | guthrie@marlboro.edu
 """
@@ -21,42 +21,90 @@ help.\nPress 'q' to quit.",
         'client' : "Next, input the specific client's name or identifier \
 (leave blank if not applicable): ",
         'platform' : "Enter the name of the platform on which this content will \
-be distributed: "
+be distributed: ",
+        'template' : "Enter a name for the template to save: "
         }
     return codes[code]
 
-def create_template(args=None):
+def create_template():
     wm = WaterMark(None)
     print("Creating a new watermark template.")
     for key in PRINTORDER:
         if key not in ['id', 'date']:
             wm.insert(key, input(cprinter(key)))
-    print(wm.dumpjson())
+    return wm
 
-def repl(args=None):
+def create_save_template(templates):
+    global TEMPPATH
+    wm = create_template()
+    wm.insert('template', input(cprinter('template')))
+    templates[wm.data['template']] = wm.data
+    return write_templates(TEMPPATH, templates)
+
+def list_templates():
+    global TEMPPATH
+    print("  ----  ")
+    print("Currently saved templates:")
+    templates = read_templates(TEMPPATH)
+    for template in templates.keys():
+        print("  - {}".format(template))
+    print("  ----  ")
+    return 0
+
+def get_template(templates):
+    template = 0
+    while template == 0:
+        name = input(\
+                "Input a template name or leave blank to create a new watermark: ")
+        if name == '': return 1
+        elif name in templates.keys(): template = templates[name]
+        else: print("No saved template with title '{}'. Please try again."\
+                    .format(name))
+    return WaterMark(template)
+
+def apply_wm_cli():
+    global TEMPPATH
+    t = get_template(read_templates(TEMPPATH))
+    if t == 1: t = create_template()
+    while True:
+        fpath = input("Enter the path to the target file: ")
+        if (file_exists(fpath) and file_type(fpath)): break
+    while True:
+        opath = input("Enter a path to save the watermarked image (leave blank \
+to replace the target file): ")
+        if opath == '':
+            opath = fpath
+            break
+        elif nfile_exists(path):
+            break
+    return write_wm(t, fpath, opath)
+
+def repl():
+    global TEMPPATH
     print(cprinter('repl'))
     cmd = input('> ')
     # 'visible' commands
     if cmd in ['n', 'new']:
-        create_template()
+        templates = read_templates(TEMPPATH)
+        return create_template(templates)
     elif cmd == 'w':
-        pass
+        apply_wm_cli()
     elif cmd == 'b':
-        pass
+        print("Batch jobs enabled soon :)")
     elif cmd in ['h', 'help']:
-        pass
+        print("Help section coming soon :)")
     elif cmd in ['q', 'exit', 'quit']:
         return 2
     # 'hidden' commands
     elif cmd in ['c', 'clear']:
         clear()
+    elif cmd in ['l', 'list']:
+        list_templates()
     else:
         err_invalid_command(cmd)
     return 0
 
 def cli(args):
-    global PRINTORDER
-    wm = WaterMark(None)
     print(cprinter('intro'))
     while repl() != 2:
         pass
